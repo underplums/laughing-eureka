db9a4422235da796d0064169bbd74adf3525db53
maybeshewillblossom <underplums@maybeshewillblossoms-MacBook-Air.local>
2026-06-01 11:20:10 +0300

    awdw

--- l.sql
+++ l.sql
@@ -1,6 +1,6 @@
 with aud as (
     select distinct
-        contact_id::int
+        contact_id::int as contact_id
     from dm.prvdr_dac_monthly
     where date_month = date'2026-02-01'
       and has_transaction_activity = 1
@@ -12,17 +12,17 @@ with aud as (
       and contact_id is not null
 ),
 
-target_by_monthly as (
+m_target as (
     select
-        aud.contact_id,
+        a.contact_id,
         case
             when d.contact_id is not null then 0
             else 1
-        end::int as target_by_monthly
-    from aud
+        end::int as target_monthly
+    from aud a
     left join (
         select distinct
-            contact_id::int
+            contact_id::int as contact_id
         from dm.prvdr_dac_monthly
         where date_month = date'2026-03-01'
           and has_transaction_activity = 1
@@ -32,55 +32,34 @@ target_by_monthly as (
               or vcoff_trn_cnt > 0
           )
           and contact_id is not null
-    ) d using(contact_id)
+    ) d
+        on a.contact_id = d.contact_id
 ),
 
-target_by_churn_table as (
+c_target as (
     select
-        aud.contact_id,
+        a.contact_id,
         case
             when c.status_churn is not null then 1
             else 0
-        end::int as target_by_churn
-    from aud
+        end::int as target_churn_table
+    from aud a
     left join pa_core_provider.prvdr_dac_churn c
-        on aud.contact_id = c.contact_id
+        on a.contact_id = c.contact_id
        and c.date_month = date'2026-03-01'
 )
 
 select
-    m.target_by_monthly,
-    c.target_by_churn,
+    m.target_monthly,
+    c.target_churn_table,
     count(*) as clients,
     count(*) * 1.0 / sum(count(*)) over () as share
-from target_by_monthly m
-join target_by_churn_table c using(contact_id)
+from m_target m
+join c_target c
+    on m.contact_id = c.contact_id
 group by
-    m.target_by_monthly,
-    c.target_by_churn
+    m.target_monthly,
+    c.target_churn_table
 order by
-    m.target_by_monthly,
-    c.target_by_churn;
-
-with aud as (
-    select distinct
-        contact_id::int
-    from dm.prvdr_dac_monthly
-    where date_month = date'2026-02-01'
-      and has_transaction_activity = 1
-      and (
-          has_mobapp_activity = 1
-          or has_pwa_activity = 1
-          or vcoff_trn_cnt > 0
-      )
-      and contact_id is not null
-)
-
-select
-    count(*) as base_audience,
-    count(c.contact_id) as found_in_churn_table,
-    count(*) - count(c.contact_id) as missing_in_churn_table
-from aud
-left join pa_core_provider.prvdr_dac_churn c
-    on aud.contact_id = c.contact_id
-   and c.date_month = date'2026-03-01';
+    m.target_monthly,
+    c.target_churn_table;
